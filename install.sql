25e31905f5feeb7d8b101a4eca9d3fa3fa753e6c
Sebastian Teumert <sebastian.teumert@gmx.de>
2012-05-23 09:16:33 +0200

    Changed install.sql

--- install.sql
+++ install.sql
@@ -3,10 +3,8 @@ DROP TABLE IF EXISTS wcf1_user_mention;
 CREATE TABLE wcf1_user_mention (
 	mentionID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	userID INT(10) NOT NULL,
-	mentionUserID INT(10) NOT NULL,
-	objectType VARCHAR(255) NOT NULL,
-	objectID INT(10) NOT NULL
-	KEY (mentionUserID)
+	mentionedUserID INT(10) NOT NULL,
+	messageTitle VARCHAR(255),
+	messageURL VARCHAR(255), -- this might be too small?
+	KEY (mentionedUserID)
 );
-
--- TODO FOREIGN KEYS?
