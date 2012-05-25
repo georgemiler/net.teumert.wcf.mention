f63efd7ad6a7154fd1ae11ffb75a1e1d36537414
Sebastian Teumert <sebastian.teumert@gmx.de>
2012-05-25 13:56:25 +0200

    Replaced messageURL with controller/id/title.

--- install.sql
+++ install.sql
@@ -1,10 +1,11 @@
 -- mention list
 DROP TABLE IF EXISTS wcf1_user_mention;
 CREATE TABLE wcf1_user_mention (
-	mentionID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	userID INT(10) NOT NULL,
+	mentionID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,	
 	mentionedUserID INT(10) NOT NULL,
-	messageTitle VARCHAR(255),
-	messageURL VARCHAR(255), -- this might be too small?
+	userID INT(10) NOT NULL DEFAULT 0, -- 0 for guests
+	controller VARCHAR(255) NOT NULL,
+	messageID INT(10) NOT NULL,
+	messageTitle VARCHAR(255),	
 	KEY (mentionedUserID)
 );
