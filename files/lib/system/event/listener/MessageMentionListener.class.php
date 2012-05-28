35fe9cd7b6fc6b026cbf2c07940484c2f5dcbe50
Sebastian Teumert <sebastian.teumert@gmx.de>
2012-05-28 10:42:41 +0200

    Updated options.xml, restructured event listener (made more methods)

--- files/lib/system/event/listener/MessageMentionListener.class.php
+++ files/lib/system/event/listener/MessageMentionListener.class.php
@@ -27,10 +27,12 @@ class MessageMentionListener extends AbstractEventListener {
 	 * @see wcf\system\event\AbstractEventListener::execute()
 	 */
 	public function execute($eventObj, $className, $eventName) {
-		if (!MODULE_MENTION)
+		if (!MODULE_MENTION && !MODULE_HASHTAGS)
 			return;
 		parent::execute($eventObj, $className, $eventName);
 	}
+	
+	/* ----- onEvent  ----- */
 
 	/**
 	 * @see wcf\form\MessageForm::readFormParameters()
@@ -46,37 +48,95 @@ class MessageMentionListener extends AbstractEventListener {
 	*/
 	public function onSave($eventObj, $className) {
 		$eventObj->text = MentionParser::getInstance()->parse($eventObj->text, 
-			$this->enableMentions && MENTION_ENABLE_MENTIONS,
-			$this->enableHashtags && MENTION_ENABLE_HASHTAGS);
+			$this->enableMentions && MODULE_MENTION,
+			$this->enableHashtags && MODULE_HASHTAGS);
 	}
 	
 	/**
 	* @see wcf\form\MessageForm::saved()
 	*/
 	public function onSaved($eventObj, $className) {
-		/* 	
-		 * KISS Principle
-		 *
-		 * If someone just wants to link @mentions, and does neither want tracking 
-		 * nor notifications, nothing needs to be done here
-		 * (no need to store mentions in db when not tracking / notificating).
-		 */ 
-		if (!MODULE_MENTION_TRACKING)
-			return;
 		
-		// at first we need to get the controller of the message
-		// this is guesswork and based on best current practice of WCF naming conventions
+		if(MENTION_TRACKING || MENTION_NOTIFICATION)
+			$this->saveMentions($eventObj);			
+		
+		if(HASHTAG_TAGGING)
+			$this->doTagging($message);		
+	}
+	
+	/**
+	* @see wcf\form\MessageForm::assignVariables()
+	*/
+	public function onAssignVariables($eventObj, $className) {
+		WCF::getTPL()->assign(array(
+			'enableMentions' => $this->enableMentions,
+			'enableHashtags' => $this->enableHashtags
+		));
+	}
+	
+	/* ----- Helper functions ----- */
+	
+	protected function saveMentions($eventObj) {
+		
+		$controller = $this->detectController();
+		$message = $this->getMessage($eventObj->objectAction);
+		
+		// object is route controller, so it can be linked and notification is possible
+		if ($message != null && $message instanceof wcf\system\request\IRouteController) {
+			$mentionedUsers = MentionParser::getInstance()->getMentions();
+		
+			foreach ($mentionedUsers as $userID => $username) {
+				$mention = UserMentionEditor::create(array(
+							'userID' => WCF::getUser()->userID,
+							'mentionedUserID' => $userID,
+							'controller' => $controller,
+							'messageID' => $message->getID(),
+							'messageTitle' => $message->getTitle()
+				));
+				if(MENTION_NOTIFICATION)
+					; // TODO fire notification
+			}
+		}		
+	}
+	
+	protected function doTagging($message) {
+		// object is taggable, so hashtags can be added as tags
+		if ($message != null && $message instanceof wcf\system\tagging\ITagged) {
+			$taggable = $message->getTaggable();
+			// TODO: get objectType (string) from $taggable->getObjectTypeID()
+			// => TagEngine::getObjectTags()
+			// => merge with hashtags
+			// => TagEngine::addObjectTags()
+			// => open question: safe way to retrieve languageID ?
+			$tags = MentionParser::getInstance()->getHashtags();
+		}
+	}
+	
+	/**
+	 * Try to detect the message controller. This is based on best current practice of WCF 
+	 * naming conventions, e.g. if the message was created in 'PostAdd', then the controller
+	 * is likely to be 'Post'.
+	 * 
+	 * @return string controller of the message or null on failure.
+	 */
+	protected function detectController() {			
 		$controller = RequestHandler::getInstance()->getActiveRequest()->getPageName();
-
-		if (StringUtil::endsWith($controller, 'Add')) 
+		
+		if (StringUtil::endsWith($controller, 'Add'))
 			$controller = StringUtil::substring($controller, 0, StringUtil::length($haystack) - 3);
 		else if (StringUtil::endsWith($controller, 'Edit'))
 			$controller = StringUtil::substring($controller, StringUtil::length($haystack) - 4, 4);
-		else // likely no valid controller
-			return;
+		else return null;
 		
+		// TODO: validate controller
+		// => check if in all dependent packages of this package a valid class can be found
+		// if that fails, return null
+		return $controller;
+	}
+	
+	protected function getMessage($objectAction) {
 		// now we need a route controller to build the route to this message
-		$message = null;		
+		$message = null;
 		if ($eventObj->objectAction->getActionName() === 'create') {
 			$returnValues = $eventObj->objectAction->getReturnValues();
 			$message = $returnValues['returnValues'];
@@ -88,43 +148,8 @@ class MessageMentionListener extends AbstractEventListener {
 			foreach ($messages as $message)
 				if ($message instanceof wcf\system\request\IRouteController)
 					UserMentionEditor::deleteAll(UserMention::getMentionIDs($controller, $message->getID()));
-				// TODO revoke unread notifications			
+			$message = null;
 		}
-		
-		// object is route controller, so it can be linked and notification is possible
-		if ($message != null && $message instanceof wcf\system\request\IRouteController) {
-			$mentionedUsers = MentionParser::getInstance()->getMentions();
-						
-			foreach ($mentionedUsers as $userID => $username) {
-				$mention = UserMentionEditor::create(array(
-					'userID' => WCF::getUser()->userID,
-					'mentionedUserID' => $userID,
-					'controller' => $controller,
-					'messageID' => $message->getID(),
-					'messageTitle' => $message->getTitle()
-					));
-				// TODO fire notification
-			}
-		}
-		
-		// object is taggable, so hashtags can be added as tags
-		if ($message != null && $message instanceof wcf\system\tagging\ITagged) {
-			$taggable = $message->getTaggable();
-			// TODO: get objectType (string) from $taggable->getObjectTypeID()
-			// => TagEngine::getObjectTags()
-			// => merge with hashtags
-			// => TagEngine::addObjectTags()
-			// => open question: safe way to retrieve languageID ?
-		}
-	}
-	
-	/**
-	* @see wcf\form\MessageForm::assignVariables()
-	*/
-	public function onAssignVariables($eventObj, $className) {
-		WCF::getTPL()->assign(array(
-			'enableMentions' => $this->enableMentions,
-			'enableHashtags' => $this->enableHashtags
-		));
+		return $message;
 	}
 }
